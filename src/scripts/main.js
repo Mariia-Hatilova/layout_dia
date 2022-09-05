5cefcc4ce532dd0bee24745e6cea266eae1157dd
Mariia Hatilova <popova.mariia17@gmail.com>
2022-09-05 16:05:28 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const contactUsForm = document.querySelector('.form-field--area');
-
-contactUsForm.addEventListener('submit', ev => {
-  ev.preventDefault();
-  contactUsForm.reset();
-});
